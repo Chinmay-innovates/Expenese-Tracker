1582fc21fad0f067a045220632511d5a3781da11
Chinmay <chinmaygope69@gmail.com>
2024-02-24 14:47:01 +0530

    Refactored

--- Expense Tracker/src/components/ExpenseTable.jsx
+++ Expense Tracker/src/components/ExpenseTable.jsx
@@ -18,43 +18,6 @@ export default function ExpenseTable({
     0
   );
 
-  const AscendingSvg = () => {
-    `
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='10'
-                  viewBox='0 0 384 512'
-                  className='arrow up-arrow'
-                  onClick={() => {
-                    setSortCallback(
-                      () => (a, b) => a.title.localeCompare(b.title);
-                    );
-                  }}
-
-                  <title>Ascending</title>
-                  <path d='M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z' />
-
-
-  `;
-  };
-
-  const DecendingSvg = () => {
-    `
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='10'
-                  viewBox='0 0 384 512'
-                  className='arrow up-arrow'
-                  onClick={() => {
-                    setSortCallback(
-                      () => (a, b) => b.title.localeCompare(a.title);
-                    );
-                  }}
-                  
-                  <title>Descending</title>
-                  <path d='M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z' />
-
-  `;
-  };
-
   function showContextMenu(e, id) {
     setMenuPosition({ left: e.clientX + 4, top: e.clientY + 4 });
     setRowId(id);
@@ -84,8 +47,32 @@ export default function ExpenseTable({
             <th className='amount-column'>
               <div>
                 <span>Title</span>
-                <AscendingSvg />
-                <DecendingSvg />
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='10'
+                  viewBox='0 0 384 512'
+                  className='arrow up-arrow'
+                  onClick={() => {
+                    () => (a, b) => a.title.localeCompare(b.title);
+                  }}
+                >
+                  <title>Ascending</title>
+                  <path d='M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z' />
+                </svg>
+
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='10'
+                  viewBox='0 0 384 512'
+                  className='arrow down-arrow'
+                  onClick={() => {
+                    () => (a, b) => b.title.localeCompare(a.title);
+                  }}
+                >
+                  <title>Descending</title>
+
+                  <path d='M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z' />
+                </svg>
               </div>
             </th>
             <th>
@@ -113,6 +100,7 @@ export default function ExpenseTable({
                   <title>Ascending</title>
                   <path d='M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z' />
                 </svg>
+
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
                   width='10'
@@ -150,17 +138,16 @@ export default function ExpenseTable({
             <th></th>
             <th>₹ {total}</th>
           </tr>
-
         </tbody>
       </table>
-          <button
-            className='clear-sort'
-            onClick={() => {
-              setSortCallback(() => () => {});
-            }}
-          >
-            Clear Sort !
-          </button>
+      <button
+        className='clear-sort'
+        onClick={() => {
+          setSortCallback(() => () => {});
+        }}
+      >
+        Clear Sort !
+      </button>
     </>
   );
 }
